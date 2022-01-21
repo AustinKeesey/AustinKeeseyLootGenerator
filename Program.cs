23c2b6ad83c1c07959218facf2c991d7f19a32e4
AustinKeesey <akeesey2023@chadwickschool.org>
2022-01-21 08:24:19 -0800

    chore: initialized project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AustinKeeseyLootGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
