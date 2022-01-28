7c3b35a652d217e838eecab5d4df013073d0ad9a
AustinKeesey <akeesey2023@chadwickschool.org>
2022-01-28 08:32:43 -0800

    2.1

--- Program.cs
+++ Program.cs
@@ -8,18 +8,32 @@ namespace AustinKeeseyLootGenerator
         {
             Console.WriteLine("Hello World!");
         }
-
-        //TODO(jcollard: 2022-01-28): You should add a method stub for each of your methods. Here is an example:
+        public static string GetValidInt()
+        {
+        // 1. Validate the prompt is a string
+        // 2. Displayt the Prompt
+        // 3. Read input from the user
+        // 4. If the user entered an invalid type, element, or theme
+        // - display an error message
+        // - go to step 2
+        // 5. Otherwise, return the users input
+        return null;
+        }
 
         /// <summary>
-        /// TODO: Complete documentation
+        /// Given a prompt to show the user, display the prompt then reads the input from the keyboard until the user enters a valid type. If the user enters an invalid type, this method displays an error message and prompts the user to try again.
         /// </summary>
         /// <param name="prompt">TODO: Complete documentation</param>
         /// <returns>TODO: Complete documentaiton</returns>
         public static int GetValidInt(string prompt)
         {
-            // TODO(jcollard: 2022-01-28): Add a step-by-step list algorithm
-            return -1;
+            // TODO 1.) Create a random loot generator
+// 2.) Generate a random number, TypeIndex, between 0 and Type.Count
+// 3.) Generate a random number, ElementIndex, between 0 and Element.Count
+// 4.) Generate a random number, ThemeIndex, between 0 and Theme.Count
+// 5.) Combine the different aspects of the loot together
+// Type[TypeIndex] + " " + Element[ElementIndex] + " " + Theme[ThemeIndex];
+// 6.) Return the loot
         }
     }
 }
